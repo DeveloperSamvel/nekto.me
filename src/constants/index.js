8398dcee762c46dfb343c281bcbd19fb89e93ee6
Samvel Vardanyan <129245937+DeveloperSamvel@users.noreply.github.com>
2024-05-27 10:17:03 +0400

    add slider in home page

---
+++ src/constants/index.js
@@ -0,0 +1,34 @@
+export const SLIDER_IMAGES = [
+  {
+    src: 'Default_Sophia_Hart_Age_30_Appearance_Tall_athletic_build_with_1.jpg',
+    alt: 'Default Sophia Hart Age 30 Appearance Tall athletic build',
+  },
+  {
+    src: 'pexels-coworking-bansko-2536000-4137612.jpg',
+    alt: 'pexels coworking bansko',
+  },
+  {
+    src: 'pexels-george-milton-6953955.jpg',
+    alt: 'pexels george milton',
+  },
+  {
+    src: 'pexels-keira-burton-6084043.jpg',
+    alt: 'pexels keira burton',
+  },
+  {
+    src: 'pexels-keira-burton-6084182.jpg',
+    alt: 'pexels keira burton',
+  },
+  {
+    src: 'pexels-michael-burrows-7129005.jpg',
+    alt: 'pexels michael burrows',
+  },
+  {
+    src: 'pexels-sam-lion-6001235.jpg',
+    alt: 'pexels sam lion',
+  },
+  {
+    src: 'pexels-shvets-production-6974793.jpg',
+    alt: 'pexels shvets production',
+  },
+];
